a8791cf4c27469fea3302d860a760a0cd9fc6946
AKASH SHARMA <109267179+Akash09090909@users.noreply.github.com>
2023-04-09 15:53:55 +0530

    Add files via upload

---
+++ Add1.java
@@ -0,0 +1,11 @@
+public class Add1
+ {
+    public static void main(String arr[])
+     {
+        int a=10,b=23,c;
+        c=a+b;
+        System.out.println("the sum of "+a+ " and " +b+ " is " +c);
+        
+    }
+    
+}
