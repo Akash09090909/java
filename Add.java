55c73efaeb8088f220dce07b0a2bd0a9b04f6adf
AKASH SHARMA <109267179+Akash09090909@users.noreply.github.com>
2023-04-09 15:53:18 +0530

    Add files via upload

---
+++ Add.java
@@ -0,0 +1,16 @@
+ class Add 
+{
+public static void main(String[] args) 
+{
+     int num1 = 23;
+      System.out.println("num ki value "+num1);
+      System.out.println("num ki value "+num1+" hai");
+    int a=23,b=34,c;
+    c=a+b;
+    System.out.println("the sum is "+c);
+    System.out.println("the sum of "+a+ " and " +b+ " is "+c );
+
+        
+    }
+    
+}
